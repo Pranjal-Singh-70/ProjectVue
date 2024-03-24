fe4c45ca4536ba8907bdd87dc8b3d96f42ebc8c5
Pranjal <s90431042@gmail.com>
2024-03-24 17:33:34 +0530

    CDN Vue

---
+++ Ind1_a/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    // template: '<h1>Template In Vue {{title}}</h2>',
+    data(){
+        return{
+            title:'T_Vue_JS'
+        }
+    }
+})
+
+app.mount('#app')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ProjectVue
 Project to Vue Starers 
+
+1- Vue can be enabled in 2 ways :
+    a- as a component via CDN
+    b- As complete Vue website 
+
